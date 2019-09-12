f94c581e5ca431d36da97e662de42de6fb3452e5
Taranarnik <pavelkurilyuk97@gmail.com>
2019-09-12 14:38:57 +0300

    added dao and dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         //create services
-
         //create items and add to shop
-
         //create user
-
         //add items to bucket
-
         //create order
-
         //watch orders story
     }
 }
