1f9093c181273ecdb6d4c1247abb784dbe9dc14c
Taranarnik <pavelkurilyuk97@gmail.com>
2019-09-24 16:30:18 +0300

    hw 17.1 solution

--- src/main/java/mate/academy/internetshop/Main.java
+++ src/main/java/mate/academy/internetshop/Main.java
@@ -10,9 +10,10 @@ import mate.academy.internetshop.service.BucketService;
 import mate.academy.internetshop.service.ItemService;
 import mate.academy.internetshop.service.OrderService;
 import mate.academy.internetshop.service.UserService;
+import org.apache.log4j.Logger;
 
 public class Main {
-
+    private static Logger logger = Logger.getLogger(Main.class);
     @Inject
     private static ItemService itemService;
     @Inject
@@ -22,7 +23,7 @@ public class Main {
         try {
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
     }
 
